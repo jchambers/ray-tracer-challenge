b505435e58f7641b50f91b06dc6587fb3e99a3c7
Jon Chambers <jon.chambers@gmail.com>
2025-08-16 11:11:57 -0400

    Use `clap` to parse command-line arguments

--- src/bin/sphere_projection.rs
+++ src/bin/sphere_projection.rs
@@ -1,3 +1,5 @@
+use std::{fs, io};
+use clap::Parser;
 use ray_tracer_challenge::canvas::Canvas;
 use ray_tracer_challenge::color::Color;
 use ray_tracer_challenge::geometry::ray::{IntersectRay, Ray};
@@ -6,32 +8,42 @@ use ray_tracer_challenge::transform;
 use ray_tracer_challenge::transform::Transformation;
 use ray_tracer_challenge::vector::Point;
 
-const CANVAS_WIDTH: usize = 1024;
-const CANVAS_HEIGHT: usize = 1024;
-
 const BACKDROP_WIDTH: f64 = 8.0;
 const BACKDROP_HEIGHT: f64 = 8.0;
 
 const CAMERA_Z: f64 = -3.0;
 const BACKDROP_Z: f64 = 3.0;
 
-fn main() {
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    out: String,
+
+    #[arg(short, long, default_value_t = 1024)]
+    size: usize,
+}
+
+fn main() -> io::Result<()> {
+    let args = Args::parse();
+
     let sphere = Sphere::new(transform::transform(&[Transformation::Translate(
         BACKDROP_WIDTH / 2.0,
         BACKDROP_HEIGHT / 2.0,
         0.0,
     )]));
+
     let camera = Point::new(BACKDROP_WIDTH / 2.0, BACKDROP_HEIGHT / 2.0, CAMERA_Z);
 
     let backdrop_color = Color::new(0.0, 0.0, 0.0);
     let hit_color = Color::new(1.0, 1.0, 1.0);
 
-    let mut canvas = Canvas::new(CANVAS_WIDTH, CANVAS_HEIGHT);
+    let mut canvas = Canvas::new(args.size, args.size);
 
-    for x_canvas in 0..CANVAS_WIDTH {
-        for y_canvas in 0..CANVAS_HEIGHT {
-            let x_world = (x_canvas as f64 / CANVAS_WIDTH as f64) * BACKDROP_WIDTH;
-            let y_world = (y_canvas as f64 / CANVAS_HEIGHT as f64) * BACKDROP_HEIGHT;
+    for x_canvas in 0..canvas.width() {
+        for y_canvas in 0..canvas.height() {
+            let x_world = (x_canvas as f64 / canvas.width() as f64) * BACKDROP_WIDTH;
+            let y_world = (y_canvas as f64 / canvas.height() as f64) * BACKDROP_HEIGHT;
 
             let target = Point::new(x_world, y_world, BACKDROP_Z);
             let camera_to_target = &target - &camera;
@@ -48,5 +60,5 @@ fn main() {
         }
     }
 
-    println!("{canvas}");
+    fs::write(args.out, format!{"{canvas}"})
 }
